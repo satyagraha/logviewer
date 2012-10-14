cdfd56b202a2a069b80d9efbc46f29cdca13df20
satyagraha <satyagraha.1956@googlemail.com>
2012-10-14 10:14:45 +0100

    Reorganized into modules.

--- logviewer-jetty/src/main/java/org/logviewer/servlet/jetty/LogViewerServlet.java
+++ logviewer-jetty/src/main/java/org/logviewer/servlet/jetty/LogViewerServlet.java
@@ -1,4 +1,4 @@
-package org.logviewer.servlet;
+package org.logviewer.servlet.jetty;
 
 import java.io.File;
 import java.io.IOException;
--- logviewer-tomcat/src/main/java/org/logviewer/servlet/tomcat/LogViewerServlet.java
+++ logviewer-tomcat/src/main/java/org/logviewer/servlet/tomcat/LogViewerServlet.java
@@ -1,4 +1,4 @@
-package org.logviewer.servlet;
+package org.logviewer.servlet.tomcat;
 
 import java.io.File;
 import java.io.IOException;
